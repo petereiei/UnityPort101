ef335b645555c20f19e544582a8c9ab8fa543f14
jirat.p <jirat.ter@gmail.com>
2021-03-04 22:06:20 +0700

    [Add] add assets games

---
+++ UnityPort101/Assets/UnityPort101/Scripts/Characters/Bases/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //module
+    #region Module
+    public CharacterAnimator characterAnimator;
+    #endregion
+
+    //method
+    #region Method
+    public abstract string GetAnimatorId();
+    #endregion
+
+    protected virtual void Awake()
+    {
+        characterAnimator = GetComponentInChildren<CharacterAnimator>();
+    }
+}
---
+++ UnityPort101/Assets/UnityPort101/Scripts/GameController/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class GameController : MonoSingleton<GameController>
+{
+    private const string PLAYER_PATH = "Prefabs/Characters/Player";
+    private GameObject area;
+
+
+    public Transform playerPoint;
+
+    
+
+    private void Awake()
+    {
+        if (area == null)
+        {
+            area = new GameObject();
+            area.name = "AreaCharacters";
+        }
+    }
+
+    private void Start()
+    {
+        GetPlayer();
+    }
+
+    private void GetPlayer()
+    {
+        GameObject player = Instantiate(Resources.Load<GameObject>(PLAYER_PATH));
+        player.transform.SetParent(area.transform);
+        player.transform.position = playerPoint.position;
+        player.transform.localScale = Vector3.one;
+    }
+}
