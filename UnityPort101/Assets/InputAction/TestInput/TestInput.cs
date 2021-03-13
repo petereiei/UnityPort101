eab5311d201cee2387ffb5379bdf3ff43bc5592c
jirat.p <jirat.ter@gmail.com>
2021-03-13 12:02:36 +0700

    [Mod] import input system

---
+++ UnityPort101/Assets/InputAction/TestInput/TestInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental;
+using UnityEngine.InputSystem;
+
+public class TestInput : MonoBehaviour
+{
+    public PlayerInput playerInput;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.Player.Movement.performed += ctx => Movement(ctx.ReadValue<Vector2>());
+    }
+
+    private void Movement(Vector2 direction)
+    {
+        // set movement game 3D, vector2 to vector3 
+        Vector3 move = new Vector3
+        {
+            x = direction.x,
+            z = direction.y
+        }.normalized;
+
+        transform.Translate(move * 5f * Time.deltaTime);
+    }
+}
