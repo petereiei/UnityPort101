2a0e54e93e248914746ec8923e9c683b80969853
jirat.p <jirat.ter@gmail.com>
2021-03-20 15:23:03 +0700

    [Add] model weapon

---
+++ UnityPort101/Assets/UnityPort101/Scripts/Characters/Enemy/EnemyCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCharacter : Character
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void Init()
+    {
+        characterAnimator.Init(this);
+    }
+
+    public override string GetCharacterId()
+    {
+        return "Enemy";
+    }
+}
---
+++ UnityPort101/Assets/UnityPort101/Scripts/Characters/WeaponSlot/WeaponSlot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSlot : MonoBehaviour
+{
+    private const string WEAPON_OBJECT_PATH = "Prefabs/Weapons/";
+
+    private string weaponId;
+    private GameObject characterWeapon;
+
+    public void Init(string weaponId)
+    {
+        this.weaponId = weaponId;
+
+        if (characterWeapon != null)
+            Destroy(characterWeapon);
+
+        GenerateWeapon();
+    }
+
+    private void GenerateWeapon()
+    {
+        GameObject weapon = Instantiate(Resources.Load<GameObject>(WEAPON_OBJECT_PATH + weaponId), transform);
+        weapon.transform.localScale = Vector3.one;
+
+        characterWeapon = weapon;
+    }
+}
